a9171cb8a427020ab56502ca2ab1761954d9191b
bushukov <byshykov@gmail.com>
2020-01-24 16:00:48 +0300

    [develop] - added cardview Bank card

--- android/app/src/main/java/mobile/technology/password_manager/activities/ActAddPassword.java
+++ android/app/src/main/java/mobile/technology/password_manager/activities/ActAddPassword.java
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import mobile.technology.password_manager.R;
@@ -11,7 +14,19 @@ import mobile.technology.password_manager.R;
 public class ActAddPassword extends AppCompatActivity {
 
     private FloatingActionButton fabSavePassword;
+    private View viewLine1, viewLine2, viewLine3, viewLine4, viewLine5;
 
+    private EditText edtPasswordName;
+    private EditText edtLogin;
+    private EditText edtPassword;
+    private EditText edtURL;
+    private EditText edtBankName;
+    private EditText edtCardNumber;
+    private EditText edtCardHolder;
+    private EditText edtCardExpiryMonth;
+    private EditText edtCardExpiryYear;
+    private EditText edtCardCVV;
+    private EditText edtCardPIN;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,5 +39,11 @@ public class ActAddPassword extends AppCompatActivity {
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setHomeButtonEnabled(true);
+
+        viewLine1 = (View)findViewById(R.id.view_line_password1);
+        viewLine2 = (View)findViewById(R.id.view_line_password2);
+        viewLine3 = (View)findViewById(R.id.view_line_password3);
+        viewLine4 = (View)findViewById(R.id.view_line_password4);
+        viewLine5 = (View)findViewById(R.id.view_line_password5);
     }
 }
