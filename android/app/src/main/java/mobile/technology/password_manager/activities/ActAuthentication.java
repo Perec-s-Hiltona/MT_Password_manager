05510ce9b79c9419c9b0bb3886858e273cf50014
bushukov <byshykov@gmail.com>
2020-05-11 14:36:57 +0300

    [develop] - added check perm SMS

--- android/app/src/main/java/mobile/technology/password_manager/activities/ActAuthentication.java
+++ android/app/src/main/java/mobile/technology/password_manager/activities/ActAuthentication.java
@@ -2,11 +2,11 @@ package mobile.technology.password_manager.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.appcompat.widget.Toolbar;
@@ -22,11 +22,14 @@ import mobile.technology.password_manager.R;
 import mobile.technology.password_manager.general.MasterAuthorization;
 import mobile.technology.password_manager.general.MasterEncrypt;
 
-public class ActAuthentication extends AppCompatActivity {
+public class ActAuthentication extends AppCompatActivity implements View.OnClickListener {
 
     private Toolbar toolbarAuthentication;
-    private TextView txtInput;
+    private TextView txtInput,txtRestorePassword, txtSendToMail, txtSendToPhone;
     private ImageView imgViewFingerprint;
+
+    private LinearLayout lnlSendPassword;
+
     private EditText edtLogin;
     private EditText edtPassword;
     private MasterEncrypt masterEncrypt;
@@ -47,63 +50,107 @@ public class ActAuthentication extends AppCompatActivity {
 
         edtLogin = (EditText)findViewById(R.id.edtLoginAuthentication);
         edtPassword = (EditText)findViewById(R.id.edtPasswordAuthentication);
-        txtInput = (TextView) findViewById(R.id.txt_input);
+
         imgViewFingerprint = (ImageView)findViewById(R.id.imView_fingerprint);
 
         masterEncrypt = new MasterEncrypt();
 
-        txtInput.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+        txtInput = (TextView) findViewById(R.id.txt_input);
+        txtRestorePassword = (TextView)findViewById(R.id.txt_restore_password);
+        txtSendToMail = (TextView)findViewById(R.id.txt_send_to_mail);
+        txtSendToPhone = (TextView)findViewById(R.id.txt_send_to_phone);
 
-                userVerification();
+        lnlSendPassword = (LinearLayout)findViewById(R.id.lnl_send_password);
+        lnlSendPassword.setVisibility(View.GONE);
 
-            }
-        });
+        // setOnClickListener
+        txtInput.setOnClickListener((View.OnClickListener) this);
+        txtRestorePassword.setOnClickListener((View.OnClickListener) this);
+        txtSendToMail.setOnClickListener((View.OnClickListener) this);
+        txtSendToPhone.setOnClickListener((View.OnClickListener) this);
+
+        imgViewFingerprint.setOnClickListener((View.OnClickListener) this);
+
+        getUserDataFromDB();
     }
 
-    private void userVerification(){
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+
+            case R.id.txt_input:
+                checkUserLoginPassword();
+                break;
+
+            case R.id.txt_restore_password:
+                lnlSendPassword.setVisibility(View.VISIBLE);
+                break;
+
+            case R.id.txt_send_to_phone:
+                sendToPhone(login, password);
+                break;
+        }
+    }
+
+    private void getUserDataFromDB() {
 
         List<UserData> userDataList = UserData.listAll(UserData.class);
 
-        for (UserData userData : userDataList){
+        if(userDataList.size() == 0){
+            finish();
+        }
+
+        for (UserData userData : userDataList) {
 
             try {
+
                 login = masterEncrypt.decryptData(userData.getLogin(), masterEncrypt.getZeroPassword());
                 password = masterEncrypt.decryptData(userData.getPassword(), masterEncrypt.getZeroPassword());
                 phone = masterEncrypt.decryptData(userData.getPhone(), masterEncrypt.getZeroPassword());
                 mail = masterEncrypt.decryptData(userData.getMail(), masterEncrypt.getZeroPassword());
 
-                if(login.equals(edtLogin.getText().toString().trim()) && password.equals(edtPassword.getText().toString().trim())){
+            } catch (Exception ex) {
 
-                    MasterAuthorization.setIsAuthorization(true);
-
-                    finish();
-
-                } else {
-
-                    final SweetAlertDialog alertDialog = new SweetAlertDialog(this, SweetAlertDialog.WARNING_TYPE);
-                    alertDialog.setTitleText((String) getResources().getText(R.string.msg_error_auth));
-                    alertDialog.setConfirmText((String) getResources().getText(R.string.msg_ok));
-
-                    alertDialog.setConfirmClickListener(new SweetAlertDialog.OnSweetClickListener() {
-                        @Override
-                        public void onClick(SweetAlertDialog sweetAlertDialog) {
-
-                            YoYo.with(Techniques.Tada).duration(1000).repeat(0).playOn(edtLogin);
-                            YoYo.with(Techniques.Tada).duration(2000).repeat(0).playOn(edtPassword);
-
-                            alertDialog.dismissWithAnimation();
-                        }
-                    });
-
-                    alertDialog.show();
-                }
-
-            } catch (Exception e) {
-
-                e.printStackTrace();
+                ex.printStackTrace();
             }
         }
+    }
+
+    private void checkUserLoginPassword(){
+        try {
+
+            if(login.equals(edtLogin.getText().toString().trim()) && password.equals(edtPassword.getText().toString().trim())){
+
+                MasterAuthorization.setIsAuthorization(true);
+                finish();
+
+            } else {
+
+                final SweetAlertDialog alertDialog = new SweetAlertDialog(this, SweetAlertDialog.WARNING_TYPE);
+                alertDialog.setTitleText((String) getResources().getText(R.string.msg_error_auth));
+                alertDialog.setConfirmText((String) getResources().getText(R.string.msg_ok));
+
+                alertDialog.setConfirmClickListener(new SweetAlertDialog.OnSweetClickListener() {
+                    @Override
+                    public void onClick(SweetAlertDialog sweetAlertDialog) {
+
+                        YoYo.with(Techniques.Tada).duration(1000).repeat(0).playOn(edtLogin);
+                        YoYo.with(Techniques.Tada).duration(2000).repeat(0).playOn(edtPassword);
+
+                        alertDialog.dismissWithAnimation();
+                    }
+                });
+
+                alertDialog.show();
+            }
+
+        } catch (Exception e) {
+
+            e.printStackTrace();
+        }
+    }
+
+    private void sendToPhone(String login, String password){
+        //TODO
     }
 }
--- android/app/src/main/java/mobile/technology/password_manager/activities/ActHome.java
+++ android/app/src/main/java/mobile/technology/password_manager/activities/ActHome.java
@@ -1,13 +1,24 @@
 package mobile.technology.password_manager.activities;
 
+import android.Manifest;
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.view.View;
 import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
 
 import com.daimajia.androidanimations.library.Techniques;
 import com.daimajia.androidanimations.library.YoYo;
+import com.google.android.material.snackbar.Snackbar;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -15,7 +26,11 @@ import java.util.List;
 import mobile.technology.password_manager.R;
 import mobile.technology.password_manager.general.MasterAuthorization;
 
+
 public class ActHome extends Activity implements View.OnClickListener  {
+
+    private static final int PERMISSION_REQUEST_CODE = 123;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,6 +60,13 @@ public class ActHome extends Activity implements View.OnClickListener  {
             Intent intent = new Intent(".act_authentication");
             startActivity(intent);
         }
+
+        if(checkAppPermissions()){
+
+            Toast.makeText(this,"HAS Per", Toast.LENGTH_SHORT).show();
+        }else {
+            requestPermissionWithRationale();
+        }
     }
 
     @Override
@@ -64,5 +86,127 @@ public class ActHome extends Activity implements View.OnClickListener  {
                 startActivity(act_settings);
                 break;
         }
+    }
+
+    //permissions requests
+    private boolean checkAppPermissions(){
+        int res = 0;
+        //string array of permitions
+        String[] permissions = new String[]{
+                Manifest.permission.SEND_SMS,
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+        };
+        for (String perms : permissions){
+            res = checkCallingOrSelfPermission(perms);
+            if (res == PackageManager.PERMISSION_DENIED){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private void requestPerms(){
+        String[] permissions = new String[]{Manifest.permission.SEND_SMS,
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_FINE_LOCATION};
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+            requestPermissions(permissions,PERMISSION_REQUEST_CODE);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        boolean allowed = true;
+
+        switch (requestCode){
+            case PERMISSION_REQUEST_CODE:
+                for (int res : grantResults){
+                    // if user granted all permissions.
+                    allowed = allowed && (res == PackageManager.PERMISSION_GRANTED);
+                }
+                break;
+            default:
+                // if user not granted permissions.
+                allowed = false;
+                break;
+        }
+        if (allowed){
+            //user granted all permissions we can perform our task.
+            /*
+            btn_SOS.setEnabled(true);
+            btn_SOS.setBackgroundColor(getResources().getColor(R.color.colorRedDark));
+            */
+        }
+        else {
+            // we will give warning to user that they haven't granted permissions.
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                if (shouldShowRequestPermissionRationale(Manifest.permission.SEND_SMS))
+                    Toast.makeText(this, R.string.perm_denied, Toast.LENGTH_LONG).show();
+                else if (shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_COARSE_LOCATION))
+                    Toast.makeText(this, R.string.perm_denied, Toast.LENGTH_LONG).show();
+                else if (shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_FINE_LOCATION))
+                    Toast.makeText(this, R.string.perm_denied, Toast.LENGTH_LONG).show();
+                else
+                    showNoStoragePermissionSnackbar();
+            }
+        }
+    }
+    public void showNoStoragePermissionSnackbar() {
+        Snackbar.make(ActHome.this.findViewById(R.id.act_home),
+                getResources().getText(R.string.perm_denied), Snackbar.LENGTH_LONG)
+                .setAction(R.string.settings, new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        openApplicationSettings();
+
+                        Toast.makeText(getApplicationContext(),
+                                R.string.open_settings_perm, Toast.LENGTH_SHORT).show();
+                    }
+                }).show();
+    }
+
+    public void openApplicationSettings() {
+        Intent appSettingsIntent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS,
+                Uri.parse("package:" + getPackageName()));
+        startActivityForResult(appSettingsIntent, PERMISSION_REQUEST_CODE);
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == PERMISSION_REQUEST_CODE) {
+            /*
+            btn_SOS.setEnabled(true);
+            btn_SOS.setBackgroundColor(getResources().getColor(R.color.colorRedDark));
+            */
+            return;
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    public void requestPermissionWithRationale() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                Manifest.permission.SEND_SMS)) {
+            showPermDesc();
+        } else if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)) {
+            showPermDesc();
+        } else if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                Manifest.permission.ACCESS_COARSE_LOCATION)) {
+            showPermDesc();
+        } else{
+            requestPerms();
+        }
+    }
+
+    private void showPermDesc(){
+        final String message = ""+ getResources().getText(R.string.perm_desc) ;
+        Snackbar.make(ActHome.this.findViewById(R.id.act_home), message, Snackbar.LENGTH_LONG)
+                .setAction(getResources().getText(R.string.permissions) , new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        requestPerms();
+                    }
+                }).show();
     }
 }
